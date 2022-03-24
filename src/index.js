e44475ddc2c2758732149f9f79c5c750bba0ef4e
owoh princewill <princewillowoh@gmail.com>
2022-03-24 12:19:20 +0100

    reducers and actions initial setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDom from "react-dom";
 import App from "./App";
+import { Provider } from "react-redux";
+import { compose, applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
+import reducers from "./reducers";
 
-ReactDom.render(<App />, document.getElementById("root"));
+//setup redux
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
